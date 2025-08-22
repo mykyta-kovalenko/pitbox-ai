13dcec197800054c9660b900fe9bb2b0a21529c3
Mykyta <mykyta.kovalenko@proarea.com>
2025-08-22 13:14:11 -0500

    add todo

--- app/tools/__init__.py
+++ app/tools/__init__.py
@@ -13,6 +13,6 @@ def get_tools() -> List[BaseTool]:
     """Get all available tools."""
     tools = []
     tools.extend(get_knowledge_tools())
-    # Add simulator tools when implemented
+    # TODO: Add simulator tools when implemented
     # tools.extend(get_simulator_tools())
     return tools
